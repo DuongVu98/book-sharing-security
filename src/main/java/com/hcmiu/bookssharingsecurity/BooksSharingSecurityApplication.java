0553721874f358d9ef8756ad2dc17219c1a081c9
Vu Tung Duong <duong.vu@bigin.vn>
2020-01-14 11:35:57 +0700

    init

---
+++ src/main/java/com/hcmiu/bookssharingsecurity/BooksSharingSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.hcmiu.bookssharingsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksSharingSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksSharingSecurityApplication.class, args);
+	}
+
+}
